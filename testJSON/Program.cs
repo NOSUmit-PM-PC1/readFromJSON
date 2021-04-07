23ed789536c534292a132bbb0ecd3252bf228e89
Makarenkomd <mahadrum@yandex.ru>
2021-04-07 16:50:54 +0300

    Add files via upload

---
+++ testJSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("data.json");
+            string strJSON = sr.ReadToEnd();
+            sr.Close();
+
+            JObject jObject = JObject.Parse(strJSON);
+
+            JToken tokGroup = jObject.GetValue("group");
+            Console.WriteLine("Группа " + tokGroup.ToString());
+
+            JToken tokStudents = jObject.GetValue("students");
+            foreach (var st in tokStudents)
+            {
+                Console.WriteLine(st.SelectToken("learn[0].subject").ToString());
+            }
+
+        }
+    }
+}
