66e9035705433d5f2c8161e063c6419fac8df490
Makarenkomd <mahadrum@yandex.ru>
2021-04-07 16:47:50 +0300

    Add files via upload

---
+++ testSimpleJSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace testSimpleJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("example.json");
+            string strJson = file.ReadToEnd();
+            file.Close();
+
+            JObject objectJSON = JObject.Parse(strJson);
+
+            JToken tokName = objectJSON.GetValue("firstName");
+            Console.WriteLine(tokName.ToString());
+
+            JToken tokCity = objectJSON.SelectToken("address.city");
+            Console.WriteLine(tokCity.ToString());
+
+            JToken tokPhones = objectJSON.GetValue("phoneNumbers");
+            foreach (var phone in tokPhones)
+                Console.WriteLine(phone);
+        }
+    }
+}
